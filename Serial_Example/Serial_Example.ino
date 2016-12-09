fecd1dc98c0fd99d6b8dbcebf88e5c4969e4cacb
Rod <sixtycycles@gmail.com>
2016-12-08 20:15:54 -0500

    yes I'm done

--- Serial_Example/Serial_Example.ino
+++ Serial_Example/Serial_Example.ino
@@ -1,7 +1,7 @@
+//Rod O'Connor for SIE558 final project. 
 int pin1 = 0;
 int pin2 = 3;
 int pin3 = 5; 
-
 
 void setup() {
   Serial.begin(9600);
@@ -9,14 +9,10 @@ void setup() {
 
 void loop() {
   
-
 int pin1read = analogRead(pin1);
 int pin2read = analogRead(pin2);
 int pin3read = analogRead(pin3);
 
- String obs = String(pin1read,DEC) + "," + String(pin2read,DEC)+ ","+String(pin3read,DEC) ; 
- Serial.println(obs);
- 
-
-  
+String obs = String(pin1read,DEC) + "," + String(pin2read,DEC)+ ","+String(pin3read,DEC) ; 
+Serial.println(obs);  
 }
